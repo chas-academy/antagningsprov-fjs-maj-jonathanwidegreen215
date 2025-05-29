0a3e2e7080489f00ec656b67a315926087a0871e
jonathanwidegreen215 <jonathanwidegreen215@hotmail.com>
2025-05-29 17:18:21 +0200

    Update uppg2.js

--- uppg2.js
+++ uppg2.js
@@ -2,7 +2,7 @@
 function uppg2() {
    // Skriv din kod här
    // deklarera en variable med ditt namn och skriv ut det i konsolen
-string name = "Jonathan";
+   var name = "Jonathan";
    console.log(name);
 
 }
