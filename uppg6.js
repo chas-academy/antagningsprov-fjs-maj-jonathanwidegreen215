b8d721d94c65e9cdeaf081b7823958366f844544
jonathanwidegreen215 <jonathanwidegreen215@hotmail.com>
2025-05-29 14:01:16 +0200

    Update uppg6.js

--- uppg6.js
+++ uppg6.js
@@ -5,7 +5,12 @@ function uppg6(){
     // Skapa en array med namn som du döper till names
     // Loopa igenom arrayen och skriv ut alla namn i konsolen
 
- 
+var namn = ["pelle", "kalle", "lars", "anders","patrik"];
+
+    for(i=0; i < namn.length; i++)
+        {
+            console.log(namn[i]);
+        }
 }
 
 module.exports = { uppg6 };
