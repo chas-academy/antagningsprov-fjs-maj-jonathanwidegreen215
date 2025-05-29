e9a12720214343d0b5bfcfae37858804007e4f50
jonathanwidegreen215 <jonathanwidegreen215@hotmail.com>
2025-05-29 13:58:40 +0200

    Update uppg5.js

--- uppg5.js
+++ uppg5.js
@@ -3,9 +3,17 @@ function uppg5() {
     // skriv en if-sats som jämför två tal
     // om det första talet är större än det andra, skriv ut "Det första talet är större än det andra"
     // annars skriv ut "Det andra talet är större än det första"
+int a = 10;
+int b = 20;
 
-
-
+if(a>b)
+{
+    console.log(a);
+}
+else
+{
+    console.log(b);
+}
 }
 
 module.exports = { uppg5 };
