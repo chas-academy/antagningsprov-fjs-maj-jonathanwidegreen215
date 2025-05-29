1eb3fbec5ff11fd5e3be073c32c1a233cc823ffe
jonathanwidegreen215 <jonathanwidegreen215@hotmail.com>
2025-05-29 13:57:07 +0200

    Update uppg4.js

--- uppg4.js
+++ uppg4.js
@@ -5,7 +5,14 @@ function uppg4() {
     // objektet ska ha två properties, name och age
     // tilldela age 25
     // skriv ut med dotnotation åldern i konsolen
+  int age;
+  string name;
+object person = {name, age};
+  name = "person";
+  age = 25;
 
+  console.log(age);
+  
 
 }
 
