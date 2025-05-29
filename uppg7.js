74d32ab823d72b9dce43b1f78123a054b670d580
jonathanwidegreen215 <jonathanwidegreen215@hotmail.com>
2025-05-29 14:03:15 +0200

    Update uppg7.js

--- uppg7.js
+++ uppg7.js
@@ -1,3 +1,10 @@
+function sumNumbers(int a, int b)
+{
+int sum = a + b
+return sum;
+}
+
+
 
 
 function uppg7() {
@@ -5,8 +12,7 @@ function uppg7() {
     // skapa en funktion som tar in två tal som argument
     // funktionen ska returnera summan av talen
     // kalla på funktionen i en return statement och ge den parametrarna 5 och 10
-
- 
+   sumNumbers(5,10);
 }
 
 module.exports = { uppg7 };
